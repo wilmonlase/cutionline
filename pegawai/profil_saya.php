5e07e6f8a621094164e70b392672c0436a8a979e
wilmonlase <wilmonlase@gmail.com>
2019-07-12 10:24:57 +0700

    -

--- pegawai/profil_saya.php
+++ pegawai/profil_saya.php
@@ -7,63 +7,32 @@
        
         <div class="form-group">
           <label><b>Nama</b></label>
-          <input type="text" name="nama_pengguna" required class="form-control" placeholder="masukkan nama pengguna">
+          <input type="text" name="nama_pengguna" required class="form-control" readonly>
         </div>
         <div class="form-group">
           <label><b>NIP</b></label>
-          <input type="text" name="nip" required class="form-control" placeholder="masukkan nama nip">
+          <input type="text" name="nip" class="form-control" readonly>
         </div>
         <div class="form-group">
           <label><b>Pangkat/Golongan Ruang</b></label>
-          <input type="text" name="gol_ruang" required class="form-control" placeholder="masukkan gol_ruang">
+          <input type="text" name="gol_ruang" class="form-control" readonly>
         </div>
         <div class="form-group">
           <label><b>Jabatan</b></label>
-          <input type="text" name="jabatan" required class="form-control" placeholder="masukkan jabatan">
+          <input type="text" name="jabatan" class="form-control" readonly>
         </div>
         <div class="form-group">
           <label><b>Unit Kerja</b></label>
-          <input type="text" name="unit_kerja" required class="form-control" placeholder="masukkan unit kerja">
+          <input type="text" name="unit_kerja" class="form-control" readonly>
         </div>
         <hr>
 
-        <button name="daftar" class="btn btn-block btn-primary" ><i class="fa fa-telegram"></i>Kembali</button>
+        <button name="kembali" class="btn btn-block btn-primary" href="index.php?halaman=halaman_utama"><i class="fa fa-telegram"></i>Kembali</button>
+        
         <br>
       </form>
 
-      <?php
-      if (isset($_POST["daftar"]))
-      {
-        $nama_pengguna = $_POST["nama_pengguna"];
-        $nama_lengkap = $_POST["nama_lengkap"];
-        $email = $_POST["email"];
-        $nomor_hp = $_POST["nomor_hp"];
-        $kata_sandi = $_POST["kata_sandi"];
-        $alamat = $_POST["alamat"];
-        $kelurahan = $_POST["kelurahan"];
-        $kecamatan = $_POST["kecamatan"];
-        $tanya = $_POST["tanya"];
-        $jawab = $_POST["jawab"];
-
-        $ambil=$koneksi->query("SELECT * FROM tbl_pelanggan WHERE email='$email'");
-        $yangcocok = $ambil->num_rows;
-        if ($yangcocok==1)
-        {
-          echo "<script>alert('Pendaftaran gagal, email sudah digunakan');</script>";
-          echo "<script>location='daftar.php';</script>";
-        }
-        else
-        {
-          $koneksi->query("INSERT INTO tbl_pelanggan
-            (id_pengguna,nama_lengkap,email,nomor_hp,kata_sandi,alamat,kelurahan,kecamatan,pertanyaan,jawaban)
-            VALUES ('$nama_pengguna','$nama_lengkap','$email','$nomor_hp','$kata_sandi','$alamat','$kelurahan','$kecamatan','$tanya','$jawab')");
-
-          echo "<script>alert('Pendaftaran sukses, silahkan login');</script>";
-          echo "<script>location='masuk.php';</script>";
-        }
-
-      }
-      ?>
+     
     </div>
   </div>
 </div>
